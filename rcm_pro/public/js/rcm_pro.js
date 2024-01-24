dd63ece38931ac357cedb79b03b8bffdc97cd2b4
Biswajit Sahoo <biswa4u85@gmail.com>
2024-01-24 13:54:17 +0530

    add css

--- rcm_pro/public/js/rcm_pro.js
+++ rcm_pro/public/js/rcm_pro.js
@@ -1,6 +1,6 @@
 $(document).ready(async function () {
   let dataMenu = await frappe.db.get_list("Rcm Menu", {
-    fields: ["name", "title", "parent_rcm_menu", "path"],
+    fields: ["name", "title", "parent_rcm_menu", "path", "is_group"],
     filters: {},
   });
   let treeData = buildTree(dataMenu);
@@ -23,32 +23,32 @@ $(document).ready(async function () {
     return tree;
   }
 
-  function renderTree(treeData, container, id = null, level = 1) {
-    let ul = $("<ul>").attr({ id: "section-" + id, class: "nested-" + level });
+  function renderTree(treeData, container, level = 1) {
+    let ul = $("<ul>").attr({ class: level > 2 ? "hidden" : "" });
     treeData.forEach(function (item) {
-      let li = $("<li>")
-        .text(item.path ? "" : item.title)
-        .attr({
-          class: level > 1 ? "collapseButton" : "",
-          "data-target": "section-" + item.name,
+      let li = $("<li>");
+      let link = $("<a>")
+        .text(item.title)
+        .attr("href", "/app/" + item.is_group == 0 ? item.path : "")
+        .click(function () {
+          if (level > 1) {
+            $(this).siblings("ul").toggleClass("hidden");
+          }
         });
-      if (item.path) {
-        let link = $("<a>")
-          .text(item.title)
-          .attr("href", "/app/" + item.path);
-        li.append(link);
-      }
       if (item.children.length > 0) {
-        renderTree(item.children, li, item.name, level + 1);
+        if (level > 1) {
+          let childLabels = `<span class="sidebar-item-icon" item-icon="getting-started">
+          <svg class="icon icon-xs"><use href="#es-line-down"></use></svg>
+          </span>`;
+          link.append(childLabels);
+        }
+      }
+      li.append(link);
+      if (item.children.length > 0) {
+        renderTree(item.children, li, level + 1);
       }
       ul.append(li);
     });
     container.append(ul);
   }
-
-  $(".nested-3").hide();
-  $(".collapseButton").click(function () {
-    let targetSection = $(this).data("target");
-    $("#" + targetSection).toggle();
-  });
 });
