ee6bdc7223d29d54360042e04c9e208586a4e076
biswa4u85 <biswa4u85@gmail.com>
2024-02-08 13:51:39 +0100

    add files

---
+++ rcm_pro/rcm_pro/doctype/sieve_analysis_of_coarse_agg_10mm/sieve_analysis_of_coarse_agg_10mm.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, erptech and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Sieve Analysis Of Coarse Agg-10MM", {
+// 	refresh(frm) {
+
+// 	},
+// });
