94d0a5555096501259ce2b72470006486d8526d2
Biswajit Sahoo <biswa4u85@gmail.com>
2024-02-14 00:59:26 +0530

    add files

--- rcm_pro/rcm_pro/doctype/sales_invoices/sales_invoices.js
+++ rcm_pro/rcm_pro/doctype/sales_invoices/sales_invoices.js
@@ -15,6 +15,27 @@ frappe.ui.form.on("Sales Invoices", {
         },
       };
     });
+    frm.set_query("order_no", function () {
+      return {
+        filters: {
+          site_name: " ",
+        },
+      };
+    });
+    frm.set_query("schedule_no", function () {
+      return {
+        filters: {
+          order_no: " ",
+        },
+      };
+    });
+    frm.set_query("challan_no", function () {
+      return {
+        filters: {
+          schedule_no: " ",
+        },
+      };
+    });
   },
   refresh: function (frm) {
     if (frm.doc.docstatus === 0) {
@@ -24,18 +45,28 @@ frappe.ui.form.on("Sales Invoices", {
       field2.$input.prop("readonly", true);
       let field3 = frm.get_field("site_address");
       field3.$input.prop("readonly", true);
+      const currentDate = new Date();
+      frm.set_value("invoice_date", currentDate);
     }
   },
+  invoice_date: async function (frm) {
+    // Set pay date
+    let invoiceDate = new Date(frm.doc.invoice_date)
+    invoiceDate.setDate(invoiceDate.getDate() + 10)
+    frm.set_value("pay_date", invoiceDate);
+  },
   party_name: async function (frm) {
-    debugger;
     // Set Address
     let addressName = await frappe.db.get_value(
       "Ledger Master",
       frm.doc.party_name,
-      "address"
+      ["address", "gst_no"]
     );
     if (addressName && addressName?.message?.address) {
       frm.set_value("party_address", addressName?.message?.address);
+    }
+    if (addressName && addressName?.message?.gst_no) {
+      frm.set_value("party_gst_no", addressName?.message?.gst_no);
     }
 
     // Set Buyer Name
@@ -53,17 +84,23 @@ frappe.ui.form.on("Sales Invoices", {
     });
   },
   buyer_name: async function (frm) {
-    // Set Address
-    let addressName = await frappe.db.get_value(
-      "Ledger Master",
-      frm.doc.buyer_name,
-      "address"
-    );
-    if (addressName && addressName?.message?.address) {
-        frm.set_value("buyer_address", addressName?.message?.address);
+    if(frm.doc.buyer_name){
+      // Set Address
+      let addressName = await frappe.db.get_value(
+        "Ledger Master",
+        frm.doc.buyer_name,
+        ["address", "gst_no"]
+      );
+      if (addressName && addressName?.message?.address) {
+          frm.set_value("buyer_address", addressName?.message?.address);
+      }
+      if (addressName && addressName?.message?.gst_no) {
+        frm.set_value("buyer_gst_no", addressName?.message?.gst_no);
+      }
     }
   },
   site_name: async function (frm) {
+    if(frm.doc.site_name){
     // Set Address
     let addressName = await frappe.db.get_value(
       "Job Site Master",
@@ -72,6 +109,59 @@ frappe.ui.form.on("Sales Invoices", {
     );
     if (addressName && addressName?.message?.address) {
         frm.set_value("site_address", addressName?.message?.address);
+    }
+    // Set Job Sites
+    frm.set_value("order_no", null);
+    frm.set_query("order_no", function () {
+      return {
+        filters: {
+          party_name: frm.doc.party_name,
+          site_name: frm.doc.site_name,
+        },
+      };
+    });
+  }
+  },
+  order_no: async function (frm) {
+    if(frm.doc.order_no){
+      // Set Job Sites
+      frm.set_value("schedule_no", null);
+      frm.set_query("schedule_no", function () {
+        return {
+          filters: {
+            order_no: frm.doc.order_no,
+          },
+        };
+      });
+    }
+  },
+  schedule_no: async function (frm) {
+    if(frm.doc.schedule_no){
+      // Set Job Sites vehicle
+      frm.set_value("challan_no", null);
+      frm.set_query("challan_no", function () {
+        return {
+          filters: {
+            schedule_no: frm.doc.schedule_no,
+          },
+        };
+      });
+    }
+  },
+  challan_no: async function (frm) {
+    if(frm.doc.challan_no){
+      // Set Vehicle No
+      let challan = await frappe.db.get_value(
+        "Delivery Challan",
+        frm.doc.challan_no,
+        ["vehicle", "driver_name"]
+      );
+      if (challan && challan?.message?.vehicle) {
+        frm.set_value("vehicle_no", challan?.message?.vehicle);
+      }
+      if (challan && challan?.message?.driver_name) {
+        frm.set_value("driver_name", challan?.message?.driver_name);
+      }
     }
   },
 });
