db05a1700e480af4ccf726187467b7e86607e238
IMMPROVED-BS <159400406+IMMPROVED-BS@users.noreply.github.com>
2024-09-12 22:31:50 +0530

    add

--- erptech_rcm/erptech_rcm/doctype/weight_bridge_master/weight_bridge_master.js
+++ erptech_rcm/erptech_rcm/doctype/weight_bridge_master/weight_bridge_master.js
@@ -6,22 +6,21 @@ frappe.ui.form.on("Weight Bridge Master", {
         doc = await frappe.db.get_doc('RCM Settings', 'enable_weigh_scale')
         if (doc.enable_weigh_scale == 1) {
             if ("serial" in navigator) {
-                // var ports = await navigator.serial.getPorts();
-                // if (ports.length == 0) {
-                frappe.confirm(
-                    'Please provide permission to connect to the weigh device',
-                    async function () {
-                        let port = await navigator.serial.requestPort();
-                        console.log('port 1', port)
-                        await port.open({ baudRate: 9600 });
-                        await listenToPort(port);
-                    },
+                var ports = await navigator.serial.getPorts();
+                if (ports.length == 0) {
+                    frappe.confirm(
+                        'Please provide permission to connect to the weigh device',
+                        async function () {
+                            let port = await navigator.serial.requestPort();
+                            await port.open({ baudRate: 9600 });
+                            await listenToPort(port, doc);
+                        },
 
-                );
-                // } else {
-                //     await ports[0].open({ baudRate: 9600 });
-                //     await listenToPort(ports[0]);
-                // }
+                    );
+                } else {
+                    await ports[0].open({ baudRate: 9600 });
+                    await listenToPort(ports[0], doc);
+                }
             }
             else {
                 frappe.msgprint("Your browser does not support serial device connection. Please switch to a supported browser to connect to your weigh device");
@@ -31,8 +30,8 @@ frappe.ui.form.on("Weight Bridge Master", {
     }
 });
 
-async function listenToPort(port) {
-    console.log('port 2', port)
+async function listenToPort(port, settings) {
+    console.log('port 2', port, settings)
     const textDecoder = new TextDecoderStream();
     const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
     const reader = textDecoder.readable.getReader();
