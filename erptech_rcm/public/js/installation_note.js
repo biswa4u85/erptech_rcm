565f32bb38de40f589b91984d38f30eedf4f6903
ERP TECH <163494723+erptechin@users.noreply.github.com>
2024-12-24 23:58:01 +0530

    upd

--- erptech_rcm/public/js/installation_note.js
+++ erptech_rcm/public/js/installation_note.js
@@ -12,10 +12,11 @@ frappe.ui.form.on('Installation Note', {
                         frm.set_value('inst_time', res.message.posting_time);
                         frm.set_value('custom_vehicle_no', res.message.custom_vehicle);
                         frm.set_value('custom_driver_name', res.message.driver);
-                        frm.set_value('custom_recipe_code', res.message.items[0].custom_bom_no);
                         frm.set_value('custom_recipe_name', res.message.items[0].item_code);
-                        // frm.doc.items[0]['custom_bom_no'] = res.message.items[0] ? res.message.items[0].bom_no : ""
-                        // frm.refresh_field('items');
+                        frm.doc.items[0]['custom_bom_no'] = res.message.items[0].custom_bom_no
+                        frm.doc.items[0]['custom_recipe_code'] = res.message.items[0].item_code
+                        frm.doc.items[0]['custom_sales_order_no'] = res.message.items[0].against_sales_order
+                        frm.refresh_field('items');
                     }
                 }
             });
