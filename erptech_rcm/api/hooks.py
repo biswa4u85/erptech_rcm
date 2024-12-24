5c80eb487b9757d4e5a537a0c9451fa5517539f0
ERP TECH <163494723+erptechin@users.noreply.github.com>
2024-12-24 22:40:28 +0530

    upd

--- erptech_rcm/api/hooks.py
+++ erptech_rcm/api/hooks.py
@@ -23,15 +23,17 @@ def create_stock_entry(doc, method):
     # Assuming you have child table "items" in the custom doctype and need to transfer items to stock
     if(doc.items[0].custom_bom_no):
         items = get_child_items(doc.items[0].custom_bom_no, "BOM", "BOM Item")
-        for item in items:            
-            stock_entry.append("items", {
-                "item_code": item.item_code,
-                "qty": item.qty,
-                "rate": item.rate,
-                "uom": item.uom,
-                "allow_zero_valuation_rate": 1 if item["item_code"] == "Water" else 0,
-                "s_warehouse": item.source_warehouse
-            })
+        for item in items: 
+            checkItem = frappe.get_doc("Item", item.item_code)    
+            if(checkItem.is_stock_item == 1):       
+                stock_entry.append("items", {
+                    "item_code": item.item_code,
+                    "qty": item.qty,
+                    "rate": item.rate,
+                    "uom": item.uom,
+                    "allow_zero_valuation_rate": 1 if item["item_code"] == "Water" else 0,
+                    "s_warehouse": item.source_warehouse
+                })
     
     # Submit the Stock Entry document to record the transaction
     stock_entry.submit()
