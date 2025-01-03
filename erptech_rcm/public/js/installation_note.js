d3af1793b703de577b77f358dc7589e34286638d
ERP TECH <163494723+erptechin@users.noreply.github.com>
2025-01-03 18:27:48 +0530

    upd

--- erptech_rcm/public/js/installation_note.js
+++ erptech_rcm/public/js/installation_note.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on('Installation Note', {
     refresh: async function (frm) {
-        if (frm.doc.items[0] && frm.doc.items[0].prevdoc_docname) {
+        if (frm.doc.name.includes("new-installation-note") && frm.doc.items[0] && frm.doc.items[0].prevdoc_docname) {
             InstallationCount = await frappe.db.count('Installation Note')
             frappe.call({
                 method: "frappe.client.get",
@@ -34,7 +34,10 @@ frappe.ui.form.on('Installation Note', {
                                     name: res.message.items[0].custom_bom_no
                                 },
                                 callback: function (res) {
+                                    const diffVal = Math.floor(Math.random() * (6 + 5 + 1)) - 5;
                                     frm.doc.custom_installation_note_recipe = []
+                                    frm.doc.custom_data = []
+                                    frm.doc.custom_installation_note_data_total = []
                                     res.message.custom_items_2.forEach((item) => {
                                         let custom_installation_note_recipe = frm.add_child("custom_installation_note_recipe");
                                         custom_installation_note_recipe.item_code = item.item_code;
@@ -44,13 +47,7 @@ frappe.ui.form.on('Installation Note', {
                                         custom_installation_note_recipe.uom = item.uom;
                                         custom_installation_note_recipe.amount = item.amount;
                                         custom_installation_note_recipe.source_warehouse = item.source_warehouse;
-                                    });
-                                    frm.refresh_field('custom_installation_note_recipe');
 
-                                    // Generate Data
-                                    frm.doc.custom_data = []
-                                    frm.doc.custom_installation_note_data_total = []
-                                    res.message.custom_items_2.forEach((item) => {
                                         let tars = 0
                                         let acts = 0
                                         for (let i = 1; i <= customNoOfBatch; i++) {
@@ -60,7 +57,6 @@ frappe.ui.form.on('Installation Note', {
                                             let tar = (item.qty / customCount) * Number(qty);
                                             custom_data.tar = tar
                                             tars = tars + Number(tar)
-                                            const diffVal = Math.floor(Math.random() * (6 + 5 + 1)) - 5;
                                             let act = Number(tar) + diffVal;
                                             custom_data.act = act
                                             acts = acts + act
@@ -71,9 +67,40 @@ frappe.ui.form.on('Installation Note', {
                                         custom_installation_note_data_total.tar = tars
                                         custom_installation_note_data_total.act = acts
                                         custom_installation_note_data_total.diff = (tars - acts) / tars * (uom == "Liter" ? 1000 : 100)
-                                    })
+
+
+                                    });
+                                    frm.refresh_field('custom_installation_note_recipe');
                                     frm.refresh_field('custom_data');
                                     frm.refresh_field('custom_installation_note_data_total');
+
+                                    // Generate Data
+                                    // frm.doc.custom_data = []
+                                    // frm.doc.custom_installation_note_data_total = []
+                                    // const diffVal = Math.floor(Math.random() * (6 + 5 + 1)) - 5;
+                                    // res.message.custom_items_2.forEach((item) => {
+                                    // let tars = 0
+                                    // let acts = 0
+                                    // for (let i = 1; i <= customNoOfBatch; i++) {
+                                    //     let custom_data = frm.add_child("custom_data");
+                                    //     custom_data.batch_no = InstallationCount + 1
+                                    //     custom_data.item = item.item_code;
+                                    //     let tar = (item.qty / customCount) * Number(qty);
+                                    //     custom_data.tar = tar
+                                    //     tars = tars + Number(tar)
+                                    //     let act = Number(tar) + diffVal;
+                                    //     custom_data.act = act
+                                    //     acts = acts + act
+                                    // }
+                                    // let custom_installation_note_data_total = frm.add_child("custom_installation_note_data_total");
+                                    // custom_installation_note_data_total.batch_no = InstallationCount + 1
+                                    // custom_installation_note_data_total.item = item.item_code;
+                                    // custom_installation_note_data_total.tar = tars
+                                    // custom_installation_note_data_total.act = acts
+                                    // custom_installation_note_data_total.diff = (tars - acts) / tars * (uom == "Liter" ? 1000 : 100)
+                                    // })
+                                    // frm.refresh_field('custom_data');
+                                    // frm.refresh_field('custom_installation_note_data_total');
                                 }
                             });
                         }
@@ -82,5 +109,5 @@ frappe.ui.form.on('Installation Note', {
             });
         }
     },
-    
+
 })
