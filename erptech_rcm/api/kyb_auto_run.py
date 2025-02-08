58865ade275f4b59b646ca67e59b974de853f0d5
biswa4u85 <biswa4u85@gmail.com>
2025-02-08 08:56:06 +0100

    upd

---
+++ erptech_rcm/api/kyb_auto_run.py
@@ -0,0 +1,109 @@
+import requests
+import pymysql
+import json
+from datetime import datetime, timedelta
+from decimal import Decimal
+import pyodbc
+
+
+def make_api_call(type):
+    """Function to make an API call and handle the response."""
+    # Replace with your API endpoint and API key if required
+    url = "https://myhome.erptech.in/api/method/"
+    headers = {"Content-Type": "application/json"}
+
+    try:
+
+        # Make the GET request
+        consumption_settings = requests.get(
+            url + "erptech_rcm.api.consumption.consumption_settings",
+            headers=headers,
+        )
+        consumption_settings.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
+
+        # Process the response
+        consumption = consumption_settings.json()  # Assuming the API returns JSON
+        settings = consumption["message"]
+        if settings:
+
+            # Make connection
+            do_connection = None
+            do_columns = None
+            if settings["sql_server"] == 1:
+                do_connection = pyodbc.connect(
+                    f"Driver={{{settings['db_driver']}}};Server={settings['db_host']};Database={settings['database']};UID={settings['db_user']};PWD={settings['db_password']};MARS_Connection=Yes;"
+                )
+            else:
+                do_connection = pymysql.connect(
+                    host=settings["db_host"],
+                    user=settings["db_user"],
+                    password=settings["db_password"],
+                    database=settings["database"],
+                    cursorclass=pymysql.cursors.DictCursor,
+                )
+
+            # Create a cursor object to interact with the database
+            do_data = do_connection.cursor()
+
+            # Example: Query the database
+            current_date = datetime.now().strftime("%Y-%m-%d")
+            currentDate = datetime.now()
+            date_before_20_days = currentDate - timedelta(days=20)
+            formatted_date = date_before_20_days.strftime("%Y-%m-%d")
+
+            do_table = settings["consumption_table_from"]
+            fetchLatest = settings["fetch_latest"]
+            from_date = formatted_date if fetchLatest else settings["from_date"]
+            to_date = current_date if fetchLatest else settings["to_date"]
+            if type == "all":
+                do_data.execute(
+                    f"SELECT * FROM {do_table} WHERE timestamp BETWEEN '{from_date}' AND '{to_date}'"
+                )
+            else:
+                do_data.execute(
+                    f"SELECT * FROM {do_table} WHERE sequence_number > {settings['count']}"
+                )
+
+            if settings["sql_server"] == 1:
+                do_columns = [column[0] for column in do_data.description]
+
+            do_results = do_data.fetchall()
+
+            if settings["sql_server"] == 1:
+                do_results = [dict(zip(do_columns, row)) for row in do_results]
+
+            results = []
+            for do_result in do_results:
+                filtered_result = {
+                    key: (
+                        value.strftime("%Y-%m-%d")
+                        if isinstance(value, datetime)
+                        else float(value) if isinstance(value, Decimal) else value
+                    )
+                    for key, value in do_result.items()
+                }
+                results.append(filtered_result)
+
+            # Make the POST request
+            consumption_set_data = requests.post(
+                url + "erptech_rcm.api.consumption.consumption_set_data_kyb",
+                json={"payload": json.dumps(results)},
+                headers=headers,
+            )
+            consumption_set_data.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
+
+            # Process the response
+            consumption = consumption_set_data.json()  # Assuming the API returns JSON
+
+            if consumption["message"]["status"] == "success":
+                # frappe.msgprint(consumption['message']['message'])
+                print(consumption["message"]["message"])
+            do_data.close()
+    except requests.exceptions.RequestException as e:
+        # Handle any exceptions
+        print("An error occurred:", e)
+
+
+# Auto-run the function when the script is executed
+if __name__ == "__main__":
+    make_api_call("all")
